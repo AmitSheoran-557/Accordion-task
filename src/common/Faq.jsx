93f2a7cb7e1f75ff47a872ef5308ba1eca695ba0
AmitSheoran-557 <amit22082006@gmail.com>
2024-12-17 14:51:10 +0530

    first commit

---
+++ src/common/Faq.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { ACCORDION_LIST } from '../components/utils/helper'
+
+const Faq = () => {
+    const [value, setFaq] = useState("")
+
+    const handleAccordion = (index) => {
+        setFaq(value === index ? "" : index)
+    }
+
+    return (
+        <div className='flex justify-center'>
+            <div className='max-w-[600px] mx-auto p-5'>
+                {ACCORDION_LIST.map((obj, i) => (
+                    <div key={i} className=' p-3 border-gray-300 border-2 rounded-lg mt-4'>
+                        <div onClick={() => handleAccordion(i)} className='flex justify-between items-center cursor-pointer'>
+                            <h3 className='text-xl font-semibold'>{obj.heading}</h3>
+                            <button className='text-2xl'>{value === i ? "-" : "+"}</button>
+                        </div>
+                        <div className={`${value === i ? "max-h-32" : "max-h-0"} sm:overflow-hidden overflow-auto transition-all duration-500 ease-in-out`}>
+                            <p>{obj.para}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Faq
---
+++ src/components/utils/helper.js
@@ -0,0 +1,21 @@
+export const ACCORDION_LIST = [
+    {
+        heading: 'Accordion Heading #one',
+        para: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+    {
+        heading: 'Accordion Heading #Two',
+        para: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        heading: 'Accordion Heading #Three',
+        para: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    },
+    {
+        heading: 'Accordion Heading #Four',
+        para: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+
+    }
+]
