16ac096245359fe88a7bb88a4794b158ae403c2d
AmitSheoran-557 <amit22082006@gmail.com>
2025-01-01 15:55:56 +0530

    edit spacing

--- src/components/PieTable.jsx
+++ src/components/PieTable.jsx
@@ -17,51 +17,51 @@ const Competition = () => {
                         </div>
                     </div>
                     <div className="xl:w-6/12 w-full flex justify-center items-center xl:mt-0 mt-8">
-                        <div className='bg-darkTeal flex xl:py-[52px] lg:py-10 py-7 ps-4 pe-2 rounded-xl overflow-auto'>
+                        <div className='bg-darkTeal flex xl:py-[52px] lg:py-10 py-7 px-4 rounded-xl overflow-auto'>
                             <div>
                                 <div>
-                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-2 opacity-0'>Null</p>
+                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-[1px] opacity-0'>Null</p>
                                 </div>
                                 <div>
-                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-2'>Mobile App</p>
+                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-[1px]'>Mobile App</p>
                                 </div>
                                 <div>
-                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-2'>Invoicing</p>
+                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-[1px]'>Invoicing</p>
                                 </div>
                                 <div>
-                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-2'>Bookkeeping</p>
+                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-[1px]'>Bookkeeping</p>
                                 </div>
                                 <div>
-                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-2'>Accountancy   support</p>
+                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-[1px]'>Accountancy   support</p>
                                 </div>
                                 <div>
-                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-2'>Tax Filing</p>
+                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-[1px]'>Tax Filing</p>
                                 </div>
                                 <div>
-                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-2'>Fee</p>
+                                    <p className='text-white py-[18px] text-sm ps-2 max-w-[130px] border-black border-t-[1px]'>Fee</p>
                                 </div>
                             </div>
                             <div>
                                 {COMPETITION_TABLE_LIST.map((item, i) => (
                                     <div className='flex w-full'>
-                                        <div className={`max-w-24 min-w-[111px] w-full border-black border-t-2 ${i === 0 ? "border-none pt-0.5 min-w-24" : ""}`}>
-                                            <div className={`mx-2 bg-white ${i === 0 ? "rounded-tl-md rounded-tr-md" : ""}  ${i === 6 ? "rounded-bl-md rounded-br-md" : ""}`}>
-                                                <p className={`mx-auto text-center text-xs font-semibold px-2 leading-[120%] flex justify-center py-[18.5px] ${i === 0 ? "border-none pt-[27px] pb-[14.3px]" : ""}  ${i === 6 ? "!min-h-[65px] !font-normal !text-[8px] max-w-[70px]" : ""}  ${i === 4 ? "!py-[28.5px]" : ""}`}>{item.icon}</p>
+                                        <div className={`max-w-24 min-w-[102px] w-full border-black border-t-[1px] ${i === 0 ? "border-none pt-0.5 min-w-24" : ""}`}>
+                                            <div className={`mx-1 bg-white ${i === 0 ? "rounded-tl-md rounded-tr-md" : ""}  ${i === 6 ? "rounded-bl-md rounded-br-md" : ""}`}>
+                                                <p className={`mx-auto text-center text-xs font-semibold px-4 leading-[120%] flex justify-center py-[18.5px] ${i === 0 ? "border-none pt-[26px] pb-[14.3px]" : ""}  ${i === 6 ? "!min-h-[65px] !font-normal !text-[8px] max-w-[70px]" : ""}  ${i === 4 ? "!py-[28.5px]" : ""}`}>{item.icon}</p>
                                             </div>
                                         </div>
-                                        <div className={`max-w-24 min-w-[110px] w-full border-black border-t-2 ${i === 0 ? "border-none pt-0.5 min-w-24" : ""}`}>
-                                            <div className={`mx-2 bg-white ${i === 0 ? "rounded-tl-md rounded-tr-md" : ""}  ${i === 6 ? "rounded-bl-md rounded-br-md" : ""}`}>
-                                                <p className={`mx-auto text-center text-xs font-semibold px-2 leading-[120%] flex justify-center py-[18.5px] ${i === 0 ? "border-none pt-[27px] pb-[14.3px]" : ""}  ${i === 6 ? "!min-h-[65px] !font-normal !text-[8px] max-w-[60px]" : ""}  ${i === 4 ? "!py-[28.5px]" : ""}`}>{item.taxscout}</p>
+                                        <div className={`max-w-24 min-w-[102px] w-full border-black border-t-[1px] ${i === 0 ? "border-none pt-0.5 min-w-24" : ""}`}>
+                                            <div className={`mx-1 bg-white ${i === 0 ? "rounded-tl-md rounded-tr-md" : ""}  ${i === 6 ? "rounded-bl-md rounded-br-md" : ""}`}>
+                                                <p className={`mx-auto text-center text-xs font-semibold px-[15px] leading-[120%] flex justify-center py-[18.5px] ${i === 0 ? "border-none pt-[26px] pb-[14.3px]" : ""}  ${i === 6 ? "!min-h-[65px] !font-normal !text-[8px] max-w-[60px]" : ""}  ${i === 4 ? "!py-[28.5px]" : ""}`}>{item.taxscout}</p>
                                             </div>
                                         </div>
-                                        <div className={`max-w-24 min-w-[110px] w-full border-black border-t-2 ${i === 0 ? "border-none pt-0.5 min-w-24" : ""}`}>
-                                            <div className={`mx-2 bg-white ${i === 0 ? "rounded-tl-md rounded-tr-md" : ""}  ${i === 6 ? "rounded-bl-md rounded-br-md" : ""}`}>
-                                                <p className={`mx-auto text-center text-xs font-semibold px-2 leading-[120%] flex justify-center py-[18.5px] ${i === 0 ? "border-none pt-[27px] pb-[14.3px]" : ""}  ${i === 6 ? "!min-h-[65px] !font-normal !text-[8px] max-w-[70px]" : ""}  ${i === 4 ? "!py-[28.5px]" : ""}`}>{item.coconut}</p>
+                                        <div className={`max-w-24 min-w-[102px] w-full border-black border-t-[1px] ${i === 0 ? "border-none pt-0.5 min-w-24" : ""}`}>
+                                            <div className={`mx-1 bg-white ${i === 0 ? "rounded-tl-md rounded-tr-md" : ""}  ${i === 6 ? "rounded-bl-md rounded-br-md" : ""}`}>
+                                                <p className={`mx-auto text-center text-xs font-semibold px-2 leading-[120%] flex justify-center py-[18.5px] ${i === 0 ? "border-none pt-[26px] pb-[14.3px]" : ""}  ${i === 6 ? "!min-h-[65px] !font-normal !text-[8px] max-w-[70px]" : ""}  ${i === 4 ? "!py-[28.5px]" : ""}`}>{item.coconut}</p>
                                             </div>
                                         </div>
-                                        <div className={`max-w-24 min-w-[110px] w-full border-black border-t-2 ${i === 0 ? "border-none pt-0.5 min-w-24" : ""}`}>
-                                            <div className={`mx-2 bg-yellowGreen ${i === 0 ? "rounded-tl-md rounded-tr-md" : ""}  ${i === 6 ? "rounded-bl-md rounded-br-md" : ""}`}>
-                                                <p className={`mx-auto text-center text-xs font-semibold px-2 leading-[120%] flex justify-center py-[18.5px] ${i === 0 ? "border-none pt-[27px] pb-[14.3px]" : ""}  ${i === 6 ? "!min-h-[65px] !font-normal !text-[8px] w-full py-[18.5px]" : ""}  ${i === 4 ? "!py-[28.5px]" : ""}`}>{item.pie}</p>
+                                        <div className={`max-w-24 !min-w-[110px] w-full border-black border-t-[1px] ${i === 0 ? "border-none pt-0.5 min-w-24" : ""}`}>
+                                            <div className={`ms-1 me-3 bg-yellowGreen ${i === 0 ? "rounded-tl-md rounded-tr-md" : ""}  ${i === 6 ? "rounded-bl-md rounded-br-md" : ""}`}>
+                                                <p className={`mx-auto text-center text-xs font-semibold px-2 leading-[120%] flex justify-center py-[18.5px] ${i === 0 ? "border-none pt-[26px] pb-[14.3px]" : ""}  ${i === 6 ? "!min-h-[65px] !font-normal !text-[8px] w-full py-[18.5px]" : ""}  ${i === 4 ? "!py-[28.5px]" : ""}`}>{item.pie}</p>
                                             </div>
                                         </div>
                                     </div>
